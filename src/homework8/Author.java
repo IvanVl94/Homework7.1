b319c6674c008bcf743cc5744b344a13c20d481f
IvanVl94 <WlasowIwan@yandex.ru>
2023-03-02 22:16:27 +0300

    Сделано ДЗ

---
+++ src/homework8/Author.java
@@ -0,0 +1,18 @@
+package homework8;
+
+import javax.naming.Name;
+
+public class Author {
+    private String name;
+    private String surname;
+    public Author (String name, String surname){
+        this.name = name;
+        this.surname = surname;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public  String getSurname(){
+        return  this.surname;
+    }
+}
